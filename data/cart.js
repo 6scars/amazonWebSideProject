5dbfe45dc47af5018b0ab2dfc57b67f713767045
marcin <zasalatol@o2.pl>
2025-04-12 02:44:47 +0200

    some jasmine expectation

--- data/cart.js
+++ data/cart.js
@@ -1,23 +1,28 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) ;
+export let cart;
 
+loadFromStorage();
 
-console.log(cart);
+export function loadFromStorage(){
+  cart = JSON.parse(localStorage.getItem('cart')) ;
 
-if(!cart){
-  cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-    deliveryOptionId: '1'
-  },{
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
-
-  saveToStorage();
+  if(!cart){
+    cart = [{
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: '1'
+    },{
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  
+    saveToStorage();
+  }
+  
 }
 
-console.log(JSON.parse(localStorage.getItem('cart')));
+
+
 
 export function addToCart(productId){
     let matchingItem=NaN;
@@ -62,7 +67,7 @@ export function removeFromCart(dataIdElement){
 
 export function saveToStorage(){
   localStorage.setItem('cart', JSON.stringify(cart));
-  cart = JSON.parse(localStorage.getItem('cart'));
+  //cart = JSON.parse(localStorage.getItem('cart'));
 
 }
 
--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,12 +1,24 @@
-import {addToCart, cart} from '../../data/cart.js'
+import {addToCart, cart, loadFromStorage} from '../../data/cart.js'
 
 describe('test suite: addToCart', ()=>{
     it('adds existing product to the cart',()=>{
 
     });
 
+
+
     it('adds a new product to the cart', ()=>{
-        addToCart('15b6fc6f-327a-4ec4-896f-486349e85a3d');
-        expect(cart.lenght).toEqual(1);
+
+        spyOn(localStorage, 'setItem');
+        spyOn(localStorage,'getItem').and.callFake(()=>{
+            return JSON.stringify([])
+        });
+        
+        loadFromStorage();
+        
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        console.log(cart)
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     });
 });
