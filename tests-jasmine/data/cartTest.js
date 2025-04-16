4636a887ea1fa178040fd087b3084ff2caad4c22
marcin <zasalatol@o2.pl>
2025-04-16 19:02:24 +0200

    tests add to carts

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -2,7 +2,22 @@ import {addToCart, cart, loadFromStorage} from '../../data/cart.js'
 
 describe('test suite: addToCart', ()=>{
     it('adds existing product to the cart',()=>{
+        spyOn(localStorage, 'setItem');
+        spyOn(localStorage,'getItem').and.callFake(()=>{
+            return JSON.stringify([{
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity:1,
+                deliveryOptionId: '1'
+            }])
+        });
+        loadFromStorage();
 
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        console.log(cart)
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(2);
     });
 
 
@@ -20,5 +35,7 @@ describe('test suite: addToCart', ()=>{
         console.log(cart)
         expect(cart.length).toEqual(1);
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(1);
     });
 });
