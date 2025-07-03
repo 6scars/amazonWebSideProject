a9af812265aecfb4b022e22795c22ef9ac240b20
marcin <zasalatol@o2.pl>
2025-07-03 18:45:09 +0200

    updateCartQuantityHeader on track side

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -1,9 +1,11 @@
 import {loadProductsFetch} from '../data/products.js';
 import {displaySummary} from './tracking/tracking.js';
+import {updateCartQuantityHeader} from './utils/quantity.js'
 loadPage()
 async function loadPage(){
     await loadProductsFetch();
 
     displaySummary();
+    updateCartQuantityHeader();
 }
 
